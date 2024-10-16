aad6114673b32b3dc494a1d23a8118f9d3fd35f1
SavioVitorAlves <saviovitor2@gmail.com>
2024-10-16 16:05:30 -0300

    docs: create class of months

---
+++ bdaynotify/lib/models/months.dart
@@ -0,0 +1,12 @@
+import 'package:bdaynotify/models/peoples.dart';
+
+class Months{
+  final String id;
+  final String name;
+  final List<People> peoples;
+  Months({
+    required this.id,
+    required this.name,
+    required this.peoples
+  });
+}
