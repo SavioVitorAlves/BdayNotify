04662789a7facebf7bf228a618ef8549e91a39b1
SavioVitorAlves <saviovitor2@gmail.com>
2024-10-31 16:10:14 -0300

    docs: create widget for chart analitycs

---
+++ bdaynotify/lib/components/charts/chart_analytics.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChartAnalytics extends StatelessWidget {
+  const ChartAnalytics({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
