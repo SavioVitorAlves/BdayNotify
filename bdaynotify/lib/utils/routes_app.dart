e7d4a4d15179e87e3bc2720337f9e8c0147682e6
SavioVitorAlves <saviovitor2@gmail.com>
2024-11-06 16:03:09 -0300

    feat: add route for screen home

--- bdaynotify/lib/utils/routes_app.dart
+++ bdaynotify/lib/utils/routes_app.dart
@@ -1,5 +1,6 @@
 class RoutesApp {
-  static const HOME = '/';
+  static const SPLASH_SCREEN = '/';
+  static const HOME = '/home';
   static const CALENDAR = '/calendar';
   static const MONTH_DETAIL = '/month_detail';
   static const MONTH_FORM = '/month-form';
